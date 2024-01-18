9b8ab195d6cb1e95980222cb2bf40e591b6a42ed
Akhilbisht798 <akhilbisht798@gmail.com>
2024-01-18 22:55:30 +0530

    changed readme.md

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
 const serverSite = import.meta.env.VITE_SERVER_URL || 'http://localhost:5000';
+console.log(serverSite)
 const socket = io(serverSite);
 
 export default socket;
